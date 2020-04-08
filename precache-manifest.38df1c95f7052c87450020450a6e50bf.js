da04bc9963013cc53c4b40718e4ed342704a52b8
Boris Vorob <borisvorob@Boriss-MacBook-Pro-2.local>
2020-04-08 10:52:38 +0300

    first commit

---
+++ precache-manifest.38df1c95f7052c87450020450a6e50bf.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03509910dbe3fec783cb01f8a3c6dfac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddff1c22ef0404668344",
+    "url": "/static/css/2.973fe934.chunk.css"
+  },
+  {
+    "revision": "4a4b9db909dfb6b09606",
+    "url": "/static/css/main.3e47a5e0.chunk.css"
+  },
+  {
+    "revision": "ddff1c22ef0404668344",
+    "url": "/static/js/2.782593df.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.782593df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a4b9db909dfb6b09606",
+    "url": "/static/js/main.40ebf935.chunk.js"
+  },
+  {
+    "revision": "4df7a6f64bcba914f32b",
+    "url": "/static/js/runtime-main.c3b99a56.js"
+  },
+  {
+    "revision": "6800f70322f1e6d82cd82306dff454b5",
+    "url": "/static/media/btc.6800f703.svg"
+  },
+  {
+    "revision": "5264665e692ace71e4f9729faa378611",
+    "url": "/static/media/copy.5264665e.svg"
+  },
+  {
+    "revision": "497d9c052f7a37388ac92ddb4971c17d",
+    "url": "/static/media/direction.497d9c05.svg"
+  },
+  {
+    "revision": "76e5d08cfb64652c9399233f32138f20",
+    "url": "/static/media/eth.76e5d08c.svg"
+  },
+  {
+    "revision": "2e8a139f276c42165170f5573ef8a7ce",
+    "url": "/static/media/loading.2e8a139f.svg"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "23fa37d7d4f78352b6579216b5091529",
+    "url": "/static/media/percent.23fa37d7.svg"
+  },
+  {
+    "revision": "fd72eb6979a455f2b9f460d84dfcb5c1",
+    "url": "/static/media/switch.fd72eb69.svg"
+  }
+]);
